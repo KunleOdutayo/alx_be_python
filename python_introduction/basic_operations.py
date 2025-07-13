90d94b84d9020450d53ad2249b1092358b3a4cda
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-07-13 15:09:25 +0100

    Mathimathical equation files for task1 complete

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 5
+x = num1 + num2
+y = num1 - num2
+z = num1 * num2
+print("Addition of", num1, "and", num2, "is", x)
+print("Subtraction of", num1, "and", num2, "is", y)
+print("Multiplication of", num1, "and", num2, "is", z)
