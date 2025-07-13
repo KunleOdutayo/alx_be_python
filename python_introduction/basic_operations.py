caf0275d3add0f2b9151fb1fc84d187baa6fe78e
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-07-13 15:12:05 +0100

    Mathimathical equation files for task1 correction

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-num1 = 10
-num2 = 5
-x = num1 + num2
-y = num1 - num2
-z = num1 * num2
-print("Addition of", num1, "and", num2, "is", x)
-print("Subtraction of", num1, "and", num2, "is", y)
-print("Multiplication of", num1, "and", num2, "is", z)
+number1 = 10
+number2 = 5
+x = number1 + number2
+y = number1 - number2
+z = number1 * number2
+print("Addition of", number1, "and", number2, "is", x)
+print("Subtraction of", number1, "and", number2, "is", y)
+print("Multiplication of", number1, "and", number2, "is", z)
