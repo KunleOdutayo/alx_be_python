166a9a5b602b70c5335d58f176971ecf5aa86250
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-03 20:40:58 +0100

    create robust division calculator

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        nom1 = float(numerator)
+        nom2 = float(denominator)
+
+        if nom2 == 0:
+            return "Cannont divide by zero."
+        
+        return nom1 / nom2
+    except ValueError:
+        return "Error: Non-numeric input. Arguements must be numbers."
