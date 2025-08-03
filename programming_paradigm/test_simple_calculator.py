18839bbf046d3e83f7d065c9d95d07b19a7e4967
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-03 21:52:42 +0100

    create simple test calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,47 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        # for two postive numbers
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(100, 200), 300)
+
+        # for two negative numbers
+        self.assertEqual(self.calc.add(-3, -2), -5)
+
+        # for a positive and a negative number
+        self.assertEqual(self.calc.add(10, -5), 5)
+        self.assertEqual(self.calc.add(-5, 5), 0)
+
+        # with zero
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(0, 90), 90)
+
+    def test_subtraction(self):
+        # for two postive numbers
+        self.assertEqual(self.calc.add(5, 3), 2)
+        self.assertEqual(self.calc.add(100, 200), -100)
+
+        # for a positive and a negative number
+        self.assertEqual(self.calc.add(10, -5), 15)
+
+        # for two negative numbers
+        self.assertEqual(self.calc.add(-3, -2), 1)
+
+         # with zero
+        self.assertEqual(self.calc.add(90, 0), 90)
+        self.assertEqual(self.calc.add(0, 90), -90)
+
+    def test_multiplication(self):
+        # for two postive numbers
+        self.assertEqual(self.calc.add(5, 3), 15)
+
+         # for a positive and a negative number
+        self.assertEqual(self.calc.add(10, -5), -50)
+
+if __name__ == '__main__':
+    inittest.main()
