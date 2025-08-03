1c77080d7e401e3a5316f0af9c0f3a34b574463d
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-03 22:39:49 +0100

    fixed library management system

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,6 +1,8 @@
 class Book:
     def __init__(self, title, author):
-        self.title = titleself.auther = authorself._is_checked_out  = False
+        self.title = title
+        self.author = author
+        self._is_checked_out  = False
 
 class Library:
     def __init__(self):
