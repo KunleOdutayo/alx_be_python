92bc05d1f2344e4bec4f27256ed542d271137c68
Kunle Odutayo <kunlay.odutayo@gmail.com>
2025-08-03 20:51:17 +0100

    edit robust division calculator

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -9,8 +9,11 @@ def main():
     numerator = sys.argv[1]
     denominator = sys.argv[2]
 
-    result = safe_divide(numerator, denominator)
-    print(result)
+    if isinstance(result, (int, float)):
+        print(f"The result of the division is {result}")
+
+    else:
+        print(result)
 
 if __name__ == "__main__":
     main()
